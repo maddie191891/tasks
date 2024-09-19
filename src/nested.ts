61bef30951be2c0439ad2ce27b096048e9a51946
Madison <mgholt@udel.edu>
2024-09-18 21:47:55 -0400

    finish for today

--- src/nested.ts
+++ src/nested.ts
@@ -104,7 +104,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const formatOptions = questions
+        .map(
+            (question) =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`,
+        )
+        .join("\n");
+    return `id,name,options,points,published\n${formatOptions}`;
 }
 
 /**
