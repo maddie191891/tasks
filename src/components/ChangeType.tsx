9df58f85b19d8384ac96adb53ee1144cc3b1e39b
Madison <mgholt@udel.edu>
2024-09-22 17:33:57 -0400

    working on components

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question",
+    );
+    const toggleQuestionType = () => {
+        setQuestionType((prevType: QuestionType) =>
+            prevType === "short_answer_question" ?
+                "multiple_choice_question"
+            :   "short_answer_question",
+        );
+    };
+    return (
+        <div>
+            <p>
+                {questionType === "multiple_choice_question" ?
+                    "Multiple Choice"
+                :   "Short Answer"}
+            </p>
+            <Button onClick={toggleQuestionType}>Change Type</Button>
+        </div>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+    return (
+        <div>
+            <Button onClick={toggleVisibility}>Reveal Answer</Button>
+            {isVisible && <div>42</div>}
+        </div>
+    );
 }
